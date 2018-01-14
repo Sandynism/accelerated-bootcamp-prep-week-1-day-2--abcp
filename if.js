b1b2bbae1d422de9bc8e872b1f9f42743fb35ae9
Sandy Yeung <sandynism@gmail.com>
2018-01-14 18:48:04 +0000

    automatically backed up by learn

--- if.js
+++ if.js
@@ -4,5 +4,14 @@ if (pokemon == "Pikachu") {
   console.log("I choose you Pikachu!!")
 }
 
-
+function overTen(number) {
+  //ifstatements
+  if (number > 10) {
+    return number
+  } else {
+    console.log("This number is not greater than 10!")
+  }
+}
+  
+  
 
